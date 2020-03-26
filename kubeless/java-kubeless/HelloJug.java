b8952ce9c51666c45c25e58cd264c8201c660dd8
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-03-26 14:48:47 +0100

    Echo the event data.

--- kubeless/java-kubeless/HelloJug.java
+++ kubeless/java-kubeless/HelloJug.java
@@ -5,6 +5,7 @@ import io.kubeless.Context;
 
 public class HelloJug {
     public String hello(io.kubeless.Event event, io.kubeless.Context context) {
+        System.out.println(event.Data);
         return "Hello JUG Hessen!";
     }
 }
